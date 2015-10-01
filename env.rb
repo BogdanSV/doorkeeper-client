4075864932fd8532e4085ef78e56d508c4d6aad3
Bogdan Barla <barla.bogdan@gmail.com>
2015-10-01 14:04:47 +0300

    Change Oauth2 config to work on the clj-vm machine

--- env.rb
+++ env.rb
@@ -1,4 +1,4 @@
 ENV['OAUTH2_CLIENT_ID']           = '9abfb27e06ca8808e4030d12712efeb2bbeaaa2e23cd403266ceb0dc829bf5c4'
 ENV['OAUTH2_CLIENT_SECRET']       = 'd827d4624a80b556ad1c8a7570eb9fd2c25a57d592f7885a543744bf6574c9fd'
-ENV['OAUTH2_CLIENT_REDIRECT_URI'] = "http://localhost:3004/callback"
-ENV['SITE']                       = "http://localhost:3003"
+ENV['OAUTH2_CLIENT_REDIRECT_URI'] = "http://clj-vm-004.3pillar.corp:3004/callback"
+ENV['SITE']                       = "http://clj-vm-004.3pillar.corp:3003"
